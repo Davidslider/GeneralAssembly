425a50db5e49f4c3dd2a08bdd3ba73dfd28821cf
Davidslider <46541772+Davidslider@users.noreply.github.com>
2019-01-14 16:40:51 -0600

    fix cardOne cardTwo

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@
 
 var cards = ["queen", "queen", "king", "king"];
 
-var cardsInPlay = [] 
-	cards.push([0])
-	console.log("User flipped queen")
+var cardsInPlay = card[1] 
 
 	cards.push([2])
 	console.log("User flipped king")
 
 
-var cardOne = cards[0]
-
-var carTwo = cards[2]
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
 	
-	if (cardsInPlay.length === 2)
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
