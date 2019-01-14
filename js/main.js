f5ac19e0bed6798cf0400c843ffb46eb6edbc655
Davidslider <46541772+Davidslider@users.noreply.github.com>
2019-01-14 17:42:08 -0600

    updated java code

--- js/main.js
+++ js/main.js
@@ -4,14 +4,21 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = card[1] 
 
-	cards.push([2])
-	console.log("User flipped king")
+
 
 
 var cardOne = cards[0];
 cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+if (cardsInPlay === cards [0])
+console.log("You found a match")
+	else {
+		console.log("pick another card")
+	}
 	
 var cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
+
+if (card.length === cards[2])
+	console.log()
