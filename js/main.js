73e53ebc86ea2bdfeff424142e43af02fe819a07
Davidslider <46541772+Davidslider@users.noreply.github.com>
2019-01-14 16:01:45 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,12 +2,16 @@
 
 var cards = ["queen", "queen", "king", "king"];
 
-var cardsInPlay = [];
-	cards.push([0]);
-	console.log("User flipped queen");
-	cards.push([2]);
-	console.log("User flipped king");
+var cardsInPlay = [] 
+	cards.push([0])
+	console.log("User flipped queen")
+
+	cards.push([2])
+	console.log("User flipped king")
+
 
 var cardOne = cards[0]
+
 var carTwo = cards[2]
 	
+	if (cardsInPlay.length === 2)
